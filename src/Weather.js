ab397fbe02deaac181c636d8963e847273c37dab
Omolola <adefilaomolola@gmail.com>
2023-12-22 05:40:39 -0800

    Added form and handleSubmit function

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,34 @@
 import React from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
 
 
-export default function Weather(props){
+
+export default function Weather(){
     function displayWeather(response) {
       alert(`Temperature in ${response.data.name} ${response.data.main.temp}⁰C`);
     }
+
+
+
+
+
+    function handleSubmit(event) {
+     event.preventDefault();
     let apiKey = "3fdc8cfbf2d6fa0116c9ae92d3df4f79";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
+    }
+    
+
+    let form = (<form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.."/>
+      <button type="submit">Search</button>
+    </form>
+    );
 
     return (
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-        wrapperStyle
-        wrapperClass
-      />
+<div>
+{form}
+</div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Weather city= "Nigeria"/>
+    <Weather/>
   </React.StrictMode>
 );
 
