182b8e6b8ead0eb22bb0452dad3cc2873a3e37c1
Omolola <adefilaomolola@gmail.com>
2023-12-22 07:25:32 -0800

    Added Gitlink component

---
+++ src/Gitlink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Gitlink() {
+    return (
+      <small>
+        <a href="https://github.com/omolola-A/weather-react.git" target="_blank">Open source code</a>
+    by Omolola Adeojo </small>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Weather from './Weather';
+import Gitlink from './Gitlink';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Weather/>
+    <br/>
+    <Gitlink/>
   </React.StrictMode>
 );
 
